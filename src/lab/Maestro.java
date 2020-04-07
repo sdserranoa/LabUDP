ee60e1b3749cceca98e339f2464886906a1abeb8
Santiago Serrano <sd.serrano@uniandes.edu.co>
2020-04-07 16:56:11 -0500

    Init

---
+++ src/lab/Maestro.java
@@ -0,0 +1,163 @@
+/**
+ * 
+ */
+package lab;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ * @author user
+ *
+ */
+public class Maestro {
+
+	private static ServerSocket ss;	
+	private static final String MAESTRO = "MAESTRO: ";
+	private static final int numeroThreads = 25;
+	private static int cont=1;
+	static Object o=new Object();
+	static Object p=new Object();
+	public static String log = "";
+	public static String ruta = "./resultados.txt";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+
+		System.out.println(MAESTRO + "Establezca puerto de conexion:");
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		int ip = Integer.parseInt(br.readLine());
+		System.out.println(MAESTRO + "Empezando servidor maestro en puerto " + ip);
+
+		File file = null;
+		String ruta = "./resultados.txt";
+
+		file = new File(ruta);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		FileWriter fw = new FileWriter(file);
+		fw.close();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+		Date date = new Date();  
+		log+="Fecha: "+formatter.format(date)+"\r\n";
+		ExecutorService executor = Executors.newFixedThreadPool(numeroThreads);
+
+		ss = new ServerSocket(ip);
+		System.out.println(MAESTRO + "Socket creado.");
+
+		byte[] bytesArray;
+
+		System.out.println("Elija un archivo para enviar (1 o 2):");
+		System.out.println("1.Archivo de texto 250MB");
+		System.out.println("2.Archivo de texto 100MB");
+		String send="";
+
+		if(br.readLine().equals("1")) {
+
+			send="./data/Archivo1.txt";
+			log+="Archivo Enviado: archivo1 \r\n";
+		}else {
+			send="./data/Archivo2.txt";
+			log+="Archivo Enviado: archivo2 \r\n";
+		}
+
+		File sendFile = new File(send);
+		bytesArray = new byte[(int) sendFile.length()]; 
+		FileInputStream fis = new FileInputStream(sendFile);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		bis.read(bytesArray,0,bytesArray.length);
+		System.out.println("¿A cuantos clientes desea enviar el archivo?");
+		String clientes=br.readLine();
+		ArrayList<Cliente> clients=new ArrayList<Cliente>();
+		try {
+			FileWriter fw1 = new FileWriter(new File(ruta),true);
+			fw1.write(log + "\r");
+			fw1.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		for (int i=0;true;i++) {
+
+			if(cont>=Integer.parseInt(clientes)) {
+				synchronized(p) {
+					p.wait();	
+				}
+				synchronized(o) {
+					o.notifyAll();
+				}
+
+			}
+			try { 
+				log="";
+				Socket sc = ss.accept();
+				System.out.println(MAESTRO + "Cliente " + i + " aceptado.");
+				log+=MAESTRO + "Cliente " + i + " aceptado.";
+				try {
+					FileWriter fw1 = new FileWriter(new File(ruta),true);
+					fw1.write(log + "\r");
+					fw1.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				Cliente d = new Cliente(sc,i,bytesArray);
+				clients.add(d);
+				executor.execute(d);
+			} catch (IOException e) {
+				System.out.println(MAESTRO + "Error creando el socket cliente.");
+				e.printStackTrace();
+			}
+
+		}
+
+
+
+	}
+
+	public static void dormir() {
+		try {
+			synchronized(o) {
+				synchronized(p) {
+
+					p.notify();
+				}
+				cont++;
+				o.wait();
+			}
+
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static synchronized void log(long t1,long t2) {
+		try {
+			log="Tiempo de envío: "+(t2-t1+"\r\n");
+			log+="Archivo recibido exitosamente \r\n";
+			FileWriter fw1 = new FileWriter(new File(ruta),true);
+			fw1.write(log + "\r");
+			fw1.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
